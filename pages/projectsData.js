b5cdcebf798472f5ec978ec31f946731453b66c7
Kriszti <letina84@gmail.com>
2023-10-23 14:19:13 +0200

    upload new project pictures

--- pages/projectsData.js
+++ pages/projectsData.js
@@ -7,14 +7,14 @@ export default [
     techDescription:" Létrehozásánál React.js-t, Material UI-t, Styled Components-t használtam.",
   },
   {
-    img1: "/images/arszamolo.png",
-    img2:"/images/memory_game.png",
+    img1: "/images/arkalkulator.png",
+    img2:"/images/arszamolo.png",
     title: "Eladási árkalkulátor",
     description: "Valós igény alapján, élelmiszerboltnak készített, árképzési folyamatot egyszerűsítő, gyorsító, a számolásból adódó hibákat megszüntető alkalmazás. A gyakorlatban jól használható.",
     techDescription:"",
   },
   {
-    img1: "/images/w_w_w.png",
+    img1: "/images/www.png",
     img2: "/images/w_w-w.png",
     title: "Webfejlesztő cég weboldala",
     description: "A weboladal, egy fiktív webfejlesztő cégnek készült.",
@@ -24,13 +24,13 @@ export default [
     img1: "/images/country_app.png",
     img2: "/images/country-app.png",
     title: "React memória játék",
-    description: "React alapjainak elsajátítása közben készítettem ezt az egyoldalas alkalmazást (SPA).",
+    description: "Az alkalmazás, API segítségével a kiválasztott kontinens országait, és annak adatait jeleníti meg.",
     techDescription:"",
   },
   {
     img: "/images/memory_game.png",
     title: "Előző portfólió oldalam.",
-    description: "React alapjainak elsajátítása közben készítettem ezt az egyoldalas alkalmazást (SPA).",
+    description: "Első portfólió oldalam.",
     techDescription:"",
   },
 ];
